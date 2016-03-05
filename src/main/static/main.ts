201ca402f17db74588d4f2c2a107635504375f1e
Stephane Maldini <smaldini@pivotal.io>
2016-03-05 15:05:59 +0000

    Pylon main

--- src/main/static/main.ts
+++ src/main/static/main.ts
@@ -1,11 +1,11 @@
 import {provide, enableProdMode} from 'angular2/core';
 import {bootstrap} from 'angular2/platform/browser';
 import {ROUTER_PROVIDERS, APP_BASE_HREF} from 'angular2/router';
-import {AppComponent} from './app/components/pylon/pylon.component';
+import {PylonComponent} from './app/components/pylon/pylon.component';
 
 if ('<%= ENV %>' === 'prod') { enableProdMode(); }
 
-bootstrap(AppComponent, [
+bootstrap(PylonComponent, [
   ROUTER_PROVIDERS,
   provide(APP_BASE_HREF, { useValue: '<%= APP_BASE %>' })
 ]);
