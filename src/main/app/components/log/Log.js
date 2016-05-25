e945f12162516e70e3d2d9ae9bcdf7d36a047e85
Damien Vitrac <dvitrac@doc4web.com>
2016-05-25 10:18:37 +0200

    Log: UI

--- src/main/app/components/log/Log.js
+++ src/main/app/components/log/Log.js
@@ -15,6 +15,8 @@
  */
 import React from 'react';
 
+require('./Log.scss');
+
 class Log extends React.Component {
 
   render() {
@@ -22,6 +24,52 @@ class Log extends React.Component {
       <div>
         <div className="heading">
           <strong>Logs</strong>
+        </div>
+        <div className="filtering">
+          <div className="search">
+            <input className="input" type="text" placeholder="Find in logs" />
+            <button type="submit" className="btn btn-primary">Search</button>
+          </div>
+        </div>
+        <div className="logs">
+          <div className="log">
+            <span className="date">16:39:08.456</span>
+            <span className="author">[hotSample]</span>
+            <span className="type"><span className="label label-info">INFO</span></span>
+            <span className="mess">pub - onSubscribe(reactor.core.publisher.EmitterProcessor$EmitterSubscriber@7326d2d7)</span>
+          </div>
+          <div className="log">
+            <span className="date">16:39:08.529</span>
+            <span className="author">[hotSample]</span>
+            <span className="type"><span className="label label-info">INFO</span></span>
+            <span className="mess">pub - request(256)</span>
+          </div>
+          <div className="log">
+            <span className="date">16:39:08.456</span>
+            <span className="author">[hotSample]</span>
+            <span className="type"><span className="label label-info">INFO</span></span>
+            <span className="mess">pub - onSubscribe(reactor.core.publisher.EmitterProcessor$EmitterSubscriber@7326d2d7)</span>
+          </div>
+          <div className="log">
+            <span className="date">16:39:08.529</span>
+            <span className="author">[hotSample]</span>
+            <span className="type"><span className="label label-info">INFO</span></span>
+            <span className="mess">pub - request(256)</span>
+          </div>
+          <div className="log error">
+            <span className="date">16:39:08.529</span>
+            <span className="author">[hotSample]</span>
+            <span className="type">
+              <span className="label label-error">ERROR</span>
+            </span>
+            <span className="mess">pub - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</span>
+          </div>
+          <div className="log warning">
+            <span className="date">16:39:08.529</span>
+            <span className="author">[hotSample]</span>
+            <span className="type"><span className="label label-warning">WARNING</span></span>
+            <span className="mess">pub - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</span>
+          </div>
         </div>
       </div>
     );
